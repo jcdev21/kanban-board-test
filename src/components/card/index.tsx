75a749ba5ee82e01015f620a1949fe1e0f9feb7b
jcdev21 <jcdev21@gmail.com>
2023-01-13 06:52:08 +0700

    feat: add component card

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
-export default function Card() {
-	return <div>Card</div>;
+type CardProps = {
+	children: React.ReactNode;
+};
+
+export default function Card({ children }: CardProps) {
+	return (
+		<div className="bg-[#FAFAFA] border border-[#E0E0E)] rounded px-2 py-4">
+			{children}
+		</div>
+	);
 }
